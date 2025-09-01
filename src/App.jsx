39f6f7a14b812cd6febaebb5e38ee33d373dbaac
sujonbiswas01 <sujonbiswasdev@gmail.com>
2025-09-01 06:13:23 +0600

    f

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,18 @@ import Index from "./routes/Index";
 import { HeroUIProvider } from "@heroui/react";
 import Preloader from "./components/PreloaderPage/Preloader";
 function App() {
-  const [preloader, setpreloader] = useState(true);
- window.addEventListener('load',()=>{
-  const s=setpreloader(false);
-  if(s){
-    console.log("false ")
-  }
-  else{
-    console.log('true')
-  }
-  
- })
+
+ 
  
   return (
     <>
-      {preloader ? (
-        <Preloader />
-      ) : (
+       
         <HeroUIProvider>
           <ThemeProvider>
             <Index />
           </ThemeProvider>
         </HeroUIProvider>
-      )}
+      
     </>
   );
 }
