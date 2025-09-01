b60856d804bdbe0278a2ae7e93e501862dfcaffc
sujonbiswas01 <sujonbiswasdev@gmail.com>
2025-09-01 06:02:08 +0600

    d

--- src/components/PreloaderPage/Preloader.jsx
+++ src/components/PreloaderPage/Preloader.jsx
@@ -3,7 +3,7 @@ import Container from '../Container'
 
 const Preloader = () => {
   return (
-    <Container className="bg-[url(/images/Loading.gif)] bg-center h-full  bg-no-repeat w-full">
+    <Container className="bg-[url(/images/Loading.gif)] bg-center h-screen m-auto bg-no-repeat w-full">
     </Container>
   )
 }
